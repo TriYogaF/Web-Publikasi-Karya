0dc122ec9097d812d983fb2219fd3071e914eb3f
Tri Yoga <tri.19038@mhs.unesa.ac.id>
2021-10-30 18:53:50 +0700

    update link view art

--- resources/views/artwork.blade.php
+++ resources/views/artwork.blade.php
@@ -20,7 +20,7 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-success fixed-top">
       <div class="container">
-        <a class="navbar-brand judul fs-4" href="index.html">Web Publikasi Karya</a>
+        <a class="navbar-brand judul fs-4" href="/">Web Publikasi Karya</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -33,13 +33,13 @@
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav ms-auto">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="">Home</a>
+              <a class="nav-link active" aria-current="page" href="/">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="artwork">Artwork</a>
+              <a class="nav-link" href="/artwork">Artwork</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="literatur">Literatur</a>
+              <a class="nav-link" href="/literatur">Literatur</a>
             </li>
             <li class="nav-item">
               <a class="btn btn-success" href="login.html" role="button"><i class="bi bi-person-square"></i></a>
