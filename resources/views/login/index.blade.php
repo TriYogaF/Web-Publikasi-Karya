052f8532c2c0ac6286ffb9bcbaa63957523128aa
Tri Yoga <tri.19038@mhs.unesa.ac.id>
2021-11-08 15:22:39 +0700

    update fitur login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -8,7 +8,8 @@
 <div class="row justify-content-center my-lg-5">
   <div class="col-lg-4 ">  
     <main class="form-signin">
-      <form>
+      <form action="/login" method="post">
+        @csrf
           <img class="mb-4" src="../assets/index/1.gif" alt="" width="72" height="72">
           <h1 class="h3 mb-3 fw-normal text-center">Log in</h1>
       
@@ -18,13 +19,27 @@
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
           </div>
           @endif
-          <div class="form-floating">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">Email address</label>
+          
+          @if (session()->has('loginError'))    
+          <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            {{ session('loginError') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
           </div>
+          @endif
+
           <div class="form-floating">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-            <label for="floatingPassword">Password</label>
+            <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" autofocus required value="{{ old('email') }}">
+            <label for="email">Email address</label>
+            @error('email')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+            @enderror
+          </div>
+
+          <div class="form-floating">
+            <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
+            <label for="password">Password</label>
           </div>
           <div class="checkbox mb-3">
             <label>
